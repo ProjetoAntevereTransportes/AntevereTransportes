cf6f9e6619ec1ec4c7b76a53c76dafaeb8005a02
lucas <lucas@lucas-Vostro-5470>
2015-05-28 21:13:35 -0300

    Alterado tamanho da barra topo do sistema. Inserido fundo dinâmico.

--- web/js/app.js
+++ web/js/app.js
@@ -2,17 +2,40 @@
     var app = angular.module("app", ["ngRoute", "inicio", "configuracao", "usuario",
         "pagamento", "ngAnimate", "pagamentoService",
         "login", "authService", "fabElement", "notifyElement",
-        "fornecedor", "fileUploadModule", "cargo", 
-        "funcionario", "cliente", "ui.utils", "pesquisaElement","Banco","Conta_Bancaria","Caminhao"]);
+        "fornecedor", "fileUploadModule", "cargo",
+        "funcionario", "cliente", "ui.utils", "pesquisaElement", "Banco", "Conta_Bancaria", "Caminhao"]);
 
     app.config(["$httpProvider", function ($httpProvider) {
             $httpProvider.interceptors.push('authInterceptorService');
         }]);
 
     app.controller('appCtrl', function ($scope, $location) {
+        $scope.shadow = false;
+        $scope.showBack = true;
+
         $scope.$on('$routeChangeStart', function (next, current) {
             $scope.mudarTitulo($location.path());
         });
+
+        $(window).scroll(function () {
+            $scope.alterarSombra();
+        });
+
+        $scope.alterarSombra = function () {
+            if (!$scope.showBack) {
+                $scope.shadow = true;
+                return;
+            }
+
+            var position = (window.pageYOffset || document.documentElement.scrollTop) - (document.documentElement.clientTop || 0);
+            if (position >= 54) {
+                $scope.shadow = true;
+            } else {
+                $scope.shadow = false;
+            }
+
+            $(".back-top").css("top", (-position * 0.5));
+        };
 
         $scope.mudarTitulo = function (path) {
             $scope.defaultColor = "rgb(69, 90, 100)";
@@ -24,6 +47,7 @@
                     $scope.title = "Início";
                     $scope.barColor = $scope.defaultColor;
                     $scope.searchColor = $scope.defaultSearchColor;
+                    $scope.showBack = true;
                     break;
                 }
                 case "/configuracao":
@@ -31,6 +55,7 @@
                     $scope.title = "Configurações";
                     $scope.barColor = $scope.defaultColor;
                     $scope.searchColor = $scope.defaultSearchColor;
+                    $scope.showBack = true;
                     break;
                 }
                 case "/usuario":
@@ -38,6 +63,7 @@
                     $scope.title = "Usuários";
                     $scope.barColor = $scope.defaultColor;
                     $scope.searchColor = $scope.defaultSearchColor;
+                    $scope.showBack = true;
                     break;
                 }
                 case "/Pagamentos":
@@ -45,6 +71,7 @@
                     $scope.title = "Pagamentos";
                     $scope.barColor = "#F44242";
                     $scope.searchColor = 'rgb(250, 116, 116)';
+                    $scope.showBack = false;
                     break;
                 }
                 case "/Login":
@@ -52,6 +79,7 @@
                     $scope.title = "Login";
                     $scope.barColor = $scope.defaultColor;
                     $scope.searchColor = $scope.defaultSearchColor;
+                    $scope.showBack = true;
                     break;
                 }
                 case "/Fornecedor":
@@ -59,6 +87,7 @@
                     $scope.title = "Fornecedores";
                     $scope.barColor = $scope.defaultColor;
                     $scope.searchColor = $scope.defaultSearchColor;
+                    $scope.showBack = true;
                     break;
                 }
                 case "/Cargo":
@@ -66,6 +95,7 @@
                     $scope.title = "Cargos";
                     $scope.barColor = $scope.defaultColor;
                     $scope.searchColor = $scope.defaultSearchColor;
+                    $scope.showBack = true;
                     break;
                 }
                 case "/Funcionario":
@@ -73,6 +103,7 @@
                     $scope.title = "Funcionários";
                     $scope.barColor = $scope.defaultColor;
                     $scope.searchColor = $scope.defaultSearchColor;
+                    $scope.showBack = true;
                     break;
                 }
                 case "/Clientes":
@@ -80,30 +111,35 @@
                     $scope.title = "Clientes";
                     $scope.barColor = $scope.defaultColor;
                     $scope.searchColor = $scope.defaultSearchColor;
+                    $scope.showBack = true;
                     break;
                 }
                 case "/Conta":
                 {
                     $scope.title = "Conta Bancaria";
-                    $scope.barColor = "#898984";
-                    $scope.searchColor = "rgb(171, 171, 171)";
+                    $scope.barColor = $scope.defaultColor;
+                    $scope.searchColor = $scope.defaultSearchColor;
+                    $scope.showBack = true;
                     break;
                 }
                 case "/Caminhao":
                 {
                     $scope.title = "Caminhão";
-                    $scope.barColor = "#898984";
-                    $scope.searchColor = "rgb(171, 171, 171)";
+                    $scope.barColor = $scope.defaultColor;
+                    $scope.searchColor = $scope.defaultSearchColor;
+                    $scope.showBack = true;
                     break;
                 }
                 case "/Banco":
                 {
                     $scope.title = "Bancos";
-                    $scope.barColor = "#898984";
-                    $scope.searchColor = "rgb(171, 171, 171)";
+                   $scope.barColor = $scope.defaultColor;
+                    $scope.searchColor = $scope.defaultSearchColor;
+                    $scope.showBack = true;
                     break;
                 }
             }
+            $scope.alterarSombra();
         };
 
         $scope.click = function (url) {
